20fe3dd87e782edae865ff7e9468d6ecc5ebd5f1
ipoliakov <ilya1291@gmail.com>
2023-06-20 17:06:00 +0300

    try to implement off-heap map

---
+++ dmap-node/src/test/java/org/ipoliakov/dmap/node/datastructures/OffHeapHashMapTest.java
@@ -0,0 +1,82 @@
+package org.ipoliakov.dmap.node.datastructures;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import com.google.protobuf.ByteString;
+
+class OffHeapHashMapTest {
+
+    @Test
+    void size_zeroAfterCreation() {
+        Map<ByteString, ByteString> map = new OffHeapHashMap(16);
+        assertEquals(map.size(), 0);
+    }
+
+    @Test
+    void size_incrementAfterPut() {
+        Map<ByteString, ByteString> map = new OffHeapHashMap(16);
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("value"));
+        assertEquals(map.size(), 1);
+    }
+
+    @Test
+    void size_decrementAfterRemove() {
+        Map<ByteString, ByteString> map = new OffHeapHashMap(16);
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("value"));
+        map.remove(ByteString.copyFromUtf8("key"));
+        assertEquals(map.size(), 0);
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void containsKey() {
+    }
+
+    @Test
+    void containsValue() {
+    }
+
+    @Test
+    void get() {
+        Map<ByteString, ByteString> map = new OffHeapHashMap(16);
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("value"));
+        ByteString key = map.get(ByteString.copyFromUtf8("key"));
+        assertEquals(key, ByteString.copyFromUtf8("value"));
+    }
+
+    @Test
+    void put() {
+
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void putAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void keySet() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void entrySet() {
+    }
+}
