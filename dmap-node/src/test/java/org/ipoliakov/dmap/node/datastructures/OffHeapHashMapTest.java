de03746de817483ca1d12241a67bd8c7f011f167
ipoliakov <ilya1291@gmail.com>
2023-06-21 16:41:30 +0300

    put/get methods

--- dmap-node/src/test/java/org/ipoliakov/dmap/node/datastructures/OffHeapHashMapTest.java
+++ dmap-node/src/test/java/org/ipoliakov/dmap/node/datastructures/OffHeapHashMapTest.java
@@ -1,6 +1,8 @@
 package org.ipoliakov.dmap.node.datastructures;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Map;
 
@@ -12,15 +14,14 @@ class OffHeapHashMapTest {
 
     @Test
     void size_zeroAfterCreation() {
-        Map<ByteString, ByteString> map = new OffHeapHashMap(16);
-        assertEquals(map.size(), 0);
+        assertEquals(0, new OffHeapHashMap(16).size());
     }
 
     @Test
     void size_incrementAfterPut() {
         Map<ByteString, ByteString> map = new OffHeapHashMap(16);
         map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("value"));
-        assertEquals(map.size(), 1);
+        assertEquals(1, map.size());
     }
 
     @Test
@@ -28,11 +29,19 @@ class OffHeapHashMapTest {
         Map<ByteString, ByteString> map = new OffHeapHashMap(16);
         map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("value"));
         map.remove(ByteString.copyFromUtf8("key"));
-        assertEquals(map.size(), 0);
+        assertEquals(0, map.size());
     }
 
     @Test
     void isEmpty() {
+        assertTrue(new OffHeapHashMap(16).isEmpty());
+    }
+
+    @Test
+    void isEmpty_falseOnNotE() {
+        Map<ByteString, ByteString> map = new OffHeapHashMap(16);
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("value"));
+        assertFalse(map.isEmpty());
     }
 
     @Test
@@ -49,16 +58,52 @@ class OffHeapHashMapTest {
         map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("value"));
         map.put(ByteString.copyFromUtf8("key2"), ByteString.copyFromUtf8("value2"));
         ByteString value = map.get(ByteString.copyFromUtf8("key"));
-        assertEquals(value, ByteString.copyFromUtf8("value"));
+        assertEquals(ByteString.copyFromUtf8("value"), value);
+        assertEquals(map.size(), 2);
     }
 
     @Test
     void put() {
+        Map<ByteString, ByteString> map = new OffHeapHashMap(16);
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("value"));
+        ByteString value = map.get(ByteString.copyFromUtf8("key"));
+        assertEquals(ByteString.copyFromUtf8("value"), value);
+        assertEquals(map.size(), 1);
+    }
 
+    @Test
+    void put_overwriteSameSizedValue() {
+        Map<ByteString, ByteString> map = new OffHeapHashMap(16);
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("valueOld"));
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("valueNew"));
+        ByteString value = map.get(ByteString.copyFromUtf8("key"));
+        assertEquals(ByteString.copyFromUtf8("valueNew"), value);
+        assertEquals(map.size(), 1);
+    }
+
+    @Test
+    void put_overwriteLessSizedValue() {
+        Map<ByteString, ByteString> map = new OffHeapHashMap(16);
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("longValue"));
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("valueNew"));
+        ByteString value = map.get(ByteString.copyFromUtf8("key"));
+        assertEquals(ByteString.copyFromUtf8("valueNew"), value);
+        assertEquals(map.size(), 1);
+    }
+
+    @Test
+    void put_overwriteBiggerSizedValue() {
+        Map<ByteString, ByteString> map = new OffHeapHashMap(16);
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("valueOld"));
+        map.put(ByteString.copyFromUtf8("key"), ByteString.copyFromUtf8("longValue"));
+        ByteString value = map.get(ByteString.copyFromUtf8("key"));
+        assertEquals(ByteString.copyFromUtf8("longValue"), value);
+        assertEquals(map.size(), 1);
     }
 
     @Test
     void remove() {
+
     }
 
     @Test
